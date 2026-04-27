ed2303e201f69310e7ba5ec33bcd919c9d69e1a1
NihadHub <ouachournihad@gmail.com>
2026-04-27 15:49:33 +0100

    first commit

---
+++ src/test/java/org/healthcare/HealthCareApplicationTests.java
@@ -0,0 +1,13 @@
+package org.healthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
